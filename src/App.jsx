0f99de787e42ec3c206cb49c41a2350642b1308a
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-15 15:50:14 +0530

    fix : fixed a App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <FavoritesProvider>
-      <Navbar />
-      <AppRoutes />
-    </FavoritesProvider>
+    <BrowserRouter>
+      <FavoritesProvider>
+        <Navbar />
+        <AppRoutes />
+      </FavoritesProvider>
+    </BrowserRouter>
   );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import MovieDetails from "../pages/MovieDetails";
 import Favorites from "../pages/Favorites";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
   );
 }

7818c20001226cd1ef2df6bb7d17427d941c7c28
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-15 15:22:57 +0530

    fix : fix a cache file

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,6 +1,6 @@
 const cache = {};
 
 export const getCache = (key) => cache[key];
-export const setCache = (key, data) => {
-  cache[key] = data;
+export const setCache = (key, value) => {
+  cache[key] = value;
 };

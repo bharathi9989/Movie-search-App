4f791290ed85e7da4b9027158248e0d0aefb3143
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-15 15:24:12 +0530

    fix : fix a movie card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 export default function MovieCard({ movie }) {
   return (
     <Link to={`/movie/${movie.imdbID}`}>
-      <div className="border p-2">
-        <img src={movie.Poster} alt={movie.Title} />
-        <h2>{movie.Title}</h2>
+      <div className="border p-2 rounded shadow hover:scale-105 transition">
+        <img
+          src={movie.Poster !== "N/A" ? movie.Poster : "/no-image.png"}
+          alt={movie.Title}
+          className="w-full h-60 object-cover"
+        />
+        <h2 className="font-bold">{movie.Title}</h2>
         <p>{movie.Year}</p>
       </div>
     </Link>

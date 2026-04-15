7dd49b54a678b412673eef12a94cb6a65b28eb3b
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-15 15:30:34 +0530

    fix : fixed app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import AppRoutes from "./routes/AppRoutes";
 import { FavoritesProvider } from "./context/FavoritesContext";
 
-function App() {
+export default function App() {
   return (
     <FavoritesProvider>
       <AppRoutes />
     </FavoritesProvider>
   );
 }
-
-export default App;
